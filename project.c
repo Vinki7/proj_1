f88025cd6a3a7e3d04512d5e45346b93ccf5f0c4
Vinki7 <xvinklers@stuba.sk>
2023-10-24 09:41:10 +0200

    implementacia funkcie v a zaciatok funkcie n

--- project.c
+++ project.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void v(FILE* fptr, char* polia/*mozno bude treba zmenit datovy typ...*/){
+void v(FILE** fptr, char*** id, char*** pozicia, char*** veliciny, char*** hodnota, char*** cas, char*** datum/*mozno bude treba zmenit datovy typ...*/){
     /*
     Najprv zistujeme stav pointeru na subor. 
         - Ak subor nebol otvoreny, skusame ho otvorit
@@ -10,37 +10,112 @@ void v(FILE* fptr, char* polia/*mozno bude treba zmenit datovy typ...*/){
                 - Ak existuju = bola volana funkcia n(), vypiseme ich obsah
                 - Ak neexistuju, vypiseme zaznamy zo suboru
     */
-    if (fptr == NULL)
+    if (*fptr == NULL)
     {   
-        //treba zmenit file
-        fptr = fopen("dataloger.txt", "r");
-        int i, len;
-        char* data_storage = (char*)calloc(50, sizeof(char)*10), break_point;
-        if (fptr == NULL){
+        *fptr = fopen("dataloger.txt", "r");
+    }
+    int i = 0, len, n_of_scanned;
+        char* data_storage = (char*)calloc(50, sizeof(char)*10);
+        if (*fptr == NULL){
             printf("Neotvoreny subor\n");
             exit(1);
-        }else if (polia == NULL)
+        }else if ((id == NULL)||(pozicia == NULL)||(veliciny == NULL)||(hodnota == NULL)||(cas == NULL)||(datum == NULL))
         {
+           while (1)
+           {
+                i++;
+                n_of_scanned = fscanf(*fptr, "%s", data_storage);
+                if (n_of_scanned == -1)
+                {
+                    break;
+                }
+                
+                switch (i)
+                {
+                    case 1:
+                        printf("ID mer. modulu: %s\n", data_storage);
+                        break;
+                    
+                    case 2:
+                        printf("Pozicia modulu: %s\n", data_storage);
+                        break;
+
+                    case 3:
+                        printf("Typ mer. veliciny: %s\n", data_storage);
+                        break;
+                    case 4:
+                        printf("Hodnota: %s\n", data_storage);
+                        break;
+                    
+                    case 5:
+                        printf("Cas merania: %s\n", data_storage);
+                        break;
+
+                    case 6:
+                        printf("Datum merania: %s\n\n", data_storage);
+                        i = 0;
+                        break;
+                }
+           }
+           
+        }
+}
+
+void n(FILE** fptr, int* pocet_zaznamov, char*** id, char*** pozicia, char*** veliciny, char*** hodnota, char*** cas, char*** datum){
+    if (*fptr == NULL)
+    {
+        printf("Neotvorený subor.");
+        exit(1);
+    }else{
+        int n_of_scanned, i;
+        char* data_storage = (char*)calloc(50, sizeof(char)*10);
         while (1)
-        {
-            for (i = 0; i < 6; i++)
-            {
-                fscanf(fptr, "%s", data_storage);
-                printf("%s\n", data_storage);
-            }
-            if (data_storage == "-1")
-            {
-                break;
-            }
-            
-        }
-        }
+           {
+                i++;
+                n_of_scanned = fscanf(*fptr, "%s", data_storage);
+                if (n_of_scanned == -1)
+                {
+                    break;
+                }
+                
+                switch (i)
+                {
+                    case 1:
+                        printf("ID mer. modulu: %s\n", data_storage);
+                        break;
+                    
+                    case 2:
+                        printf("Pozicia modulu: %s\n", data_storage);
+                        break;
+
+                    case 3:
+                        printf("Typ mer. veliciny: %s\n", data_storage);
+                        break;
+                    case 4:
+                        printf("Hodnota: %s\n", data_storage);
+                        break;
+                    
+                    case 5:
+                        printf("Cas merania: %s\n", data_storage);
+                        break;
+
+                    case 6:
+                        printf("Datum merania: %s\n\n", data_storage);
+                        i = 0;
+                        break;
+                }
+           }
+        
+        
     }
+    
 }
 
 int main(void){
     FILE *ptr_dataloger = NULL;
-    char* zaznam_poli = NULL;//mozno bude treba zmenit datovy typ...
-    v(ptr_dataloger, zaznam_poli);
+    int pocet_zaznamov = 0;
+    char ***zaznam_id = NULL, ***zaznam_pozicia = NULL, ***zaznam_veliciny = NULL, ***zaznam_hodnot = NULL, ***zaznam_casov = NULL, ***zaznam_datumov = NULL;//mozno bude treba zmenit datovy typ...
+    v(&ptr_dataloger, zaznam_id, zaznam_pozicia, zaznam_veliciny, zaznam_hodnot, zaznam_casov, zaznam_datumov);
+    n(&ptr_dataloger, &pocet_zaznamov, zaznam_id, zaznam_pozicia, zaznam_veliciny, zaznam_hodnot, zaznam_casov, zaznam_datumov);
     return 0;
 }
