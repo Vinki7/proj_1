adbfab37a70081d26fbc3b4dcc638dcdfef3ccc2
Vinki7 <xvinklers@stuba.sk>
2023-10-26 09:31:41 +0200

    vyriesenei problemu segmentation_fault u strcpy(), bolo treba dodefinovat strukturu

--- demo.c
+++ demo.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){
-    char* value = malloc(sizeof(char)*50);
-    char values[6];
-    value = "A";
-    values[0] = *value;
-
+    char **values =NULL;
+    values = (char**)malloc(sizeof(char*)*50);
+    for (int i = 0; i < 50; i++)
+    {
+        *values= (char*)malloc(sizeof(char)*50);
+    }
+    
+    char* value = (char*)malloc(sizeof(char)*50);
+    value = "Arr";
+    strcpy(*values, value);
+    printf("%s", *values);
     return 0;
 }
