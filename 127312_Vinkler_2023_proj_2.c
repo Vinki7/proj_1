bc6ecc93ec9ecf610bb667135f54defabe38ee50
Vinki7 <xvinklers@stuba.sk>
2023-11-10 17:19:15 +0100

    The basic contstruction of main

---
+++ 127312_Vinkler_2023_proj_2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char *id;
+    char **ptr_id; 
+    ptr_id = &(id);
+}
+int main(void){
+    printf("Zadajte príkaz:\n");
+    char prikaz;
+    while (1)
+    {
+        scanf(" %c", &prikaz);
+        switch (prikaz)
+        {
+        case 'v':
+            
+            break;
+        
+        case 'n':
+            
+            break;
+
+        case 'c':
+            
+            break;
+
+        case 's':
+            
+            break;
+
+        case 'h':
+            
+            break;
+        
+        case 'z':
+            
+            break;
+        
+        case 'k':
+            
+            break;
+        default:
+            printf("Zadali ste nedefinovaný príkaz, skúste to prosím znovu...\n");
+        }
+        printf("Zadajte ďalší príkaz...\n");
+    }
+    return 0;
+}
